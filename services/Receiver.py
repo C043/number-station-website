d022d1e6b07c9a608a0e2a1a332adfcd03db307c
C043 <mariofragnitoph@gmail.com>
2026-04-29 16:23:51 +0200

    Adds abstract Receiver class

---
+++ services/Receiver.py
@@ -0,0 +1,34 @@
+from abc import ABC, abstractmethod
+from enum import Enum
+from typing import Protocol
+
+
+class Mode(str, Enum):
+    AM = "am"
+    FM = "fm"
+    USB = "usb"
+    LSB = "lsb"
+    CW = "cw"
+
+
+class AudioStream(Protocol):
+    def read(self):
+        return
+
+
+class Receiver(ABC):
+    @abstractmethod
+    def set_mode(self, mode: Mode):
+        raise NotImplementedError
+
+    @abstractmethod
+    def set_frequency(self, frequency: float):
+        raise NotImplementedError
+
+    @abstractmethod
+    def set_filter(self, lower_bound: float, upper_bound: float):
+        raise NotImplementedError
+
+    @abstractmethod
+    def audioStream(self) -> AudioStream:
+        raise NotImplementedError
